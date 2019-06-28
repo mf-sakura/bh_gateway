2955e42bdefc046199e3d6e4a39d946c4f5e178e
mf-sakura <sakura.yuto@moneyforward.co.jp>
2019-06-28 14:58:35 +0900

    add opentracing

--- app/server/connection.go
+++ app/server/connection.go
@@ -5,6 +5,7 @@ import (
 	"github.com/mf-sakura/bh_gateway/app/config"
 	"google.golang.org/grpc"
 
+	"github.com/grpc-ecosystem/go-grpc-middleware/tracing/opentracing"
 	hpb "github.com/mf-sakura/bh_gateway/app/proto/hotel"
 	upb "github.com/mf-sakura/bh_gateway/app/proto/user"
 )
@@ -15,11 +16,11 @@ var hotelClient hpb.HotelServiceClient
 func CreateConnections(conf *config.GRPCConfig) error {
 	userTarget := fmt.Sprintf("%s:%s", conf.UserHost, conf.UserPort)
 	hotelTarget := fmt.Sprintf("%s:%s", conf.HotelHost, conf.HotelPort)
-	uconn, err := grpc.Dial(userTarget, grpc.WithInsecure())
+	uconn, err := grpc.Dial(userTarget, grpc.WithInsecure(), grpc.WithUnaryInterceptor(grpc_opentracing.UnaryClientInterceptor()))
 	if err != nil {
 		return err
 	}
-	hconn, err := grpc.Dial(hotelTarget, grpc.WithInsecure())
+	hconn, err := grpc.Dial(hotelTarget, grpc.WithInsecure(), grpc.WithUnaryInterceptor(grpc_opentracing.UnaryClientInterceptor()))
 	if err != nil {
 		return err
 	}
